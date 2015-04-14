fc9f3de2d67cb6d22f0a8ab81b303be4bfbf1a51
MaximeHemion <mhemion@csumb.edu>
2015-04-13 17:40:21 -0800

    still not working

--- JS/Markers.js
+++ JS/Markers.js
@@ -1,3 +1,6 @@
+
+
+
 Parse.initialize("wFFZue6UImviqCkCtBlLazQ51N39l3TUy04u2i8l", "hyPckU5KOyjTh2AZPdKs1zUYSYlAkbcHC3XC0gOz");
 //map settings/initializers changed here
 var map; // global map variable
@@ -94,3 +97,29 @@ google.maps.event.addDomListener(window, 'load', function() {
   initialize();
   populateMap();
 });
+
+var main = function() {
+	  /* Push the body and the nav over by 285px over */
+	  $('.icon-menu').click(function() {
+		$('.menu').animate({
+		  left: "0px"
+		}, 200);
+
+		$('#map-canvas').animate({
+		  left: "285px"
+		}, 200);
+	  });
+
+	  /* Then push them back */
+	  $('.icon-close').click(function() {
+		$('.menu').animate({
+		  left: "-285px"
+		}, 200);
+
+		$('#map-canvas').animate({
+		  left: "0px"
+		}, 200);
+	  });
+	};
+$(document).ready(main);
+
