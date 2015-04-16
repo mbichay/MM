501928c123516cce68942c7570dec4be604bfc6a
MaximeHemion <mhemion@csumb.edu>
2015-04-15 16:09:09 -0800

    MOAR MENU
    FOR THE GOD OF MENUS

--- JS/Markers.js
+++ JS/Markers.js
@@ -98,28 +98,5 @@ google.maps.event.addDomListener(window, 'load', function() {
   populateMap();
 });
 
-var main = function() {
-	  /* Push the body and the nav over by 285px over */
-	  $('.icon-menu').click(function() {
-		$('.menu').animate({
-		  left: "0px"
-		}, 200);
 
-		$('#map-canvas').animate({
-		  left: "285px"
-		}, 200);
-	  });
-
-	  /* Then push them back */
-	  $('.icon-close').click(function() {
-		$('.menu').animate({
-		  left: "-285px"
-		}, 200);
-
-		$('#map-canvas').animate({
-		  left: "0px"
-		}, 200);
-	  });
-	};
-$(document).ready(main);
 
